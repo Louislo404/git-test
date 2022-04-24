e4c0ad05f8831dea7640b86ffe706b1bab9fbacf
Louis <Lllremain@163.com>
2022-04-24 14:54:05 +0800

    second commit

--- src/main/java/com/luo/git/GitTest.java
+++ src/main/java/com/luo/git/GitTest.java
@@ -5,8 +5,6 @@ package com.luo.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
-        System.out.println("hello git!");
-        System.out.println("hello git!");
+        System.out.println("first change");
     }
 }
