5d948aadab7a4866d9ad2d186a71bd1c13dc752f
Louis <Lllremain@163.com>
2022-04-24 14:18:50 +0800

    first commit

---
+++ src/main/java/com/luo/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.luo.git;
+
+/**
+ * @author louis
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
